19d0822309cdd526944c9935edbed37d5a08e673
Atharva <bajajatharv7@gmail.com>
2026-08-29 21:03:56 +0530

    Time: 12 ms (16.22%), Space: 13.1 MB (58.66%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -3,7 +3,7 @@ public:
     int maxVowels(string s, int k) {//classic fixed length question of sliding window 
         int count=0;
         int countmax=0;
-        for(int i=0;i<k;i++){
+        for(int i=0;i<k;i++){//rembember this
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
                 count++;
             }
@@ -12,11 +12,11 @@ public:
         if(count==k){
             return k;
         }
-        for(int i=k;i<s.length();i++){
+        for(int i=k;i<s.length();i++){//rembember
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
                 count++;
             }
-            if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u'){
+            if(s[i-k]=='a' || s[i-k]=='e' || s[i-k]=='i' || s[i-k]=='o' || s[i-k]=='u'){//rembember
                 count--;
             }
             countmax=max(countmax,count);
